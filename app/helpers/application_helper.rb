3cec769e6486a24db406dad577e7af096afe55f9
prashanthpereira <prashanth.pereira@gmail.com>
2012-09-18 01:01:16 -0300

    Update app/helpers/application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,35 +1,41 @@
-<%= form_for(@post) do |f| %>
-  <% if @post.errors.any? %>
-    <div id="error_explanation">
-      <h2><%= pluralize(@post.errors.count, "error") %> prohibited this post from being saved:</h2>
+module ApplicationHelper
 
-      <ul>
-      <% @post.errors.full_messages.each do |msg| %>
-        <li><%= msg %></li>
-      <% end %>
-      </ul>
-    </div>
-  <% end %>
+  def admin?
+    if current_user.permission_level == "admin" || current_user.id == "admin"
+      return true
+    else
+      return false
+    end
+  end
+
+  def owner?(id)
+    if current_user.id == id
+      return true
+    else
+      return false
+    end
+  end
+
+  def admin_or_owner?(id)
+    if (admin? || owner?(id))
+      return true
+    else
+      return false
+    end
+  end
 
 
-  <div class="field">
-    <%= f.label :category %><br />
-    <select name="categories" id="categorylist">
-      <%= options_for_select [
-                                     ['general',           'general'],
-                                     ['important',          'important']
+  def get_all_comments_for_post(postid)
+    return  Post.where( :id => post.parent_id).sort_by_column_decreasing(:updated_at)
+  end
 
-                             ] %>
-    </select>
+  def get_user(userid)
+    @result = User.select(:username).where(:id => userid)
+     @result.each do |res|
+        return res.username
+      end
+  end
 
-  </div>
 
-    <div class="field">
-      <%= f.label :content %><br />
-      <%= f.text_field :content, :size => "100" %>
-    </div>
 
-  <div class="actions">
-    <%= f.submit %>
-  </div>
-<% end %>
+end
