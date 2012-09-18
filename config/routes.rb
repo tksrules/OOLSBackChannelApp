2fd019a1693194db5479b4c5880466dcfbf818ab
Prashanth Pereira <prashanth.pereira@gmail.com>
2012-09-18 04:43:30 -0400

    new changes

--- config/routes.rb
+++ config/routes.rb
@@ -68,4 +68,5 @@ OOLSBackChannelApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match '/update_vote' => 'votes#update_vote', :as => 'update_vote'
 end
